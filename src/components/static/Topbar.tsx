1326e843d3d1d998e745a4eb297465bd61558f6d
Muhammad Zain-ul-Abdin Ansari <zainzabid4338@gmail.com>
2024-07-24 11:03:08 +0500

    fix bug

--- src/components/static/Topbar.tsx
+++ src/components/static/Topbar.tsx
@@ -3,7 +3,7 @@ import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
-  DropdownMenuLabel,
+  // DropdownMenuLabel,
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
